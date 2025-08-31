f3629072989c26cc00326ce24972efc3c971ff2e
DESKTOP-8M6N9IK\PC <nndungcse@gmail.com>
2025-08-31 21:04:12 +0700

    Get started page

--- FrontEnd/src/app/pages/landing/landing.ts
+++ FrontEnd/src/app/pages/landing/landing.ts
@@ -1,18 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
-import { DividerModule } from 'primeng/divider';
-import { RippleModule } from 'primeng/ripple';
-import { StyleClassModule } from 'primeng/styleclass';
-import { HeroWidget } from './components/herowidget';
-import { TopbarWidget } from './components/topbarwidget.component';
-import { ListTop } from './components/list/list-top';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   templateUrl: 'landing.html',
-  imports: [RouterModule, TopbarWidget, HeroWidget, RippleModule, StyleClassModule, ButtonModule, DividerModule, ListTop],
+  imports: [CommonModule, ButtonModule],
   template: ``
 })
 export class Landing {}
